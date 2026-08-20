d17844f3942ebef9aecbe171f26426807c99739f
Raghav Purohit <raghavpurohit711@gmail.com>
2026-08-21 00:45:17 +0530

    Set up core backend architecture and prediction routes

--- Used Car Arbitrage/src/app/api.py
+++ Used Car Arbitrage/src/app/api.py
@@ -1,11 +1,18 @@
-from fastapi import FastAPI
+from fastapi import FastAPI,HTTPException
 from pydantic import BaseModel
 import joblib
 import pandas as pd
+import os
 
 app = FastAPI(title="Used Car Arbitrage API")
 
-model = joblib.load(r"C:\Programming\Machine Learning\Used Car Price Intelligence\Used Car Arbitrage\models\arbitrage_model.pkl")
+current_dir = os.path.dirname(os.path.abspath(__file__))
+model_path = os.path.join(current_dir,"..","..","models","arbitrage_model.pkl")
+
+if not os.path.exists(model_path):
+    model_path = 'models/arbitrage_model.pkl'
+
+model = joblib.load(model_path)
 
 class CarFeatures(BaseModel):
     Year : int
@@ -18,19 +25,22 @@ class CarFeatures(BaseModel):
 @app.post("/predict")
 
 def predict_price(car : CarFeatures):
+    try:
 
-    input_data = pd.DataFrame([{
-        "Year":car.Year,
-        "Clean_Kilometers" : car.Clean_Kilometers,
-        "Brand" : car.Brand,
-        "Model" : car.Model,
-        "Fuel_Type" : car.Fuel_type,
-        "Transmission" : car.Transmission
-    }])
+        input_data = pd.DataFrame([{
+            "Year":car.Year,
+            "Clean_Kilometers" : car.Clean_Kilometers,
+            "Brand" : car.Brand,
+            "Model" : car.Model,
+            "Fuel_Type" : car.Fuel_type,
+            "Transmission" : car.Transmission
+        }])
 
-    prediction = model.predict(input_data)[0]
+        prediction = model.predict(input_data)[0]
 
-    return {"Predicted Price": int(prediction)}
+        return {"Predicted Price": int(prediction)}
+    except Exception as e:
+        raise HTTPException(status_code=400, detail=f"Prediction Failed:{str(e)}")
 
 @app.get("/")
 
